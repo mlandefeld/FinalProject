5b357ebf5371431017a6b57d08a6144ae649a6eb
Margaret Landefeld <mlandefeld@users.noreply.github.com>
2016-05-24 20:49:57 -0400

    added contact form, models, and controllers for contact and project tables

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 namespace FinalProject.Controllers
 {
@@ -26,5 +28,37 @@ namespace FinalProject.Controllers
 
             return View();
         }
+        /*  //http://www.mikesdotnetting.com/article/268/how-to-send-email-in-asp-net-mvc   alter web.config to send email
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("recipient@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("sender@outlook.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "mlandefeld@gmail.com",  
+                        Password = "password"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+            */
     }
 }
